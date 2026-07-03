561650d59e930f2eaefb5daf4f16c46f6d4e5f88
M. Abdullah Butt <abutt2210@gmail.com>
2026-07-03 22:27:23 +0400

    fix: add favicon link to index.html and update font imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from './App.tsx'
 import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/500.css'
 import '@fontsource/poppins/600.css'
+import '@fontsource-variable/merriweather/index.css'
 import './styles/fonts.css'
 import 'katex/dist/katex.min.css'
-import 'highlight.js/styles/atom-one-dark.css'
 import './styles/index.css'
 
 const rootElement = document.getElementById('root')
